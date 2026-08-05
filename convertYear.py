91e0f0557eb13805ecc4cad1071b853a1592c292
417-20-witchanakan <56414@yupparaj.ac.th>
2026-08-05 11:42:37 +0700

    Fix variable names and update input prompts

--- convertYear.py
+++ convertYear.py
@@ -1,6 +1,6 @@
 import streamlit as st
-st.title("แอปพิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
+st.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
 
-bh_year= st.number_input ("กรอกปี พ.ศ. 2554" ,value=2554)
-ce_year= bh_years-543
+bh_year= st.number_input ("กรอกปี พ.ศ. ที่ต้องการแปลง" ,value=2554)
+ce_year= bh_year-543
 st.header(f"ปี ค.ศ. คือ : {ce_year}")
