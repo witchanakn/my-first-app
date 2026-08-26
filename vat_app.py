f78b6200bb9271282e410233b80075be6d1e6036
417-20-witchanakan <56414@yupparaj.ac.th>
2026-08-26 11:22:14 +0700

    Create VAT calculation app using Streamlit
    Add a Streamlit application for VAT calculation.

---
+++ vat_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+import streamlit as st
+price = st.number_input("219 (บาท):", value=0.0)
+net_price = price - vat
+st.write("นาย วิชญ์ชนกันต์ ทาโน เลขที่ 20  ม.4/17")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
