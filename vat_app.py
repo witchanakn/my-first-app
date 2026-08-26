e06f3534b774b7a0f479853e079aa20dd402acf5
417-20-witchanakan <56414@yupparaj.ac.th>
2026-08-26 11:26:10 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-price = st.number_input("กรอกราคาสินค้า (บาท):", value=7.0)
+price = st.number_input("219 (บาท):", value=219)
 import streamlit as st
-price = st.number_input("219 (บาท):", value=7.0)
+price = st.number_input("219 (บาท):", value=219)
 net_price = price - vat
 st.write("นาย วิชญ์ชนกันต์ ทาโน เลขที่ 20  ม.4/17")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
